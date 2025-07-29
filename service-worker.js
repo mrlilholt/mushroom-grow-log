1918825e3715527b79086bb0930955c41e5333aa
Addison <alilholt@baldwinschool.org>
2025-07-28 20:06:42 -0400

    Initial commit - Mushroom Grow Log PWA

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,38 @@
-const CACHE_NAME = "mushroom-grow-log-v1";
+const CACHE_VERSION = 'v2'; // Increment this version number for updates
+const CACHE_NAME = `mushroom-grow-log-${CACHE_VERSION}`;
 const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./service-worker.js"
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
+// Install: Cache files
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
+      console.log("Caching new files");
       return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting(); // Activate new service worker immediately
 });
 
+// Activate: Remove old caches
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim(); // Take control of all pages
+});
+
+// Fetch: Serve from cache first, fallback to network
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
